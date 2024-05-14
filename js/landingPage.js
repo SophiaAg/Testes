6508873833e4aceeb3e3ed9ef4f93dea48cfd9ec
SophiaAg <sophiafroez7@gmail.com>
2024-05-14 01:38:09 -0300

    01H40

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,19 +1,19 @@
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
-
+ 
 let carouselDom = document.querySelector('.carousel');
 let SliderDom = carouselDom.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-
+ 
 let timeDom = document.querySelector('.carousel .time');
-
+ 
 let timeRunning = 3000;
 let timeAutoNext = 7000;
-
+ 
 nextDom.onclick = function(){
     showSlider('next');    
 }
-
+ 
 prevDom.onclick = function(){
     showSlider('prev');    
 }
@@ -24,7 +24,7 @@ let runNextAuto = setTimeout(() => {
 function showSlider(type){
     let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
+   
     if(type === 'next'){
         SliderDom.appendChild(SliderItemsDom[0]);
         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
@@ -39,24 +39,38 @@ function showSlider(type){
         carouselDom.classList.remove('next');
         carouselDom.classList.remove('prev');
     }, timeRunning);
-
+ 
     clearTimeout(runNextAuto);
     runNextAuto = setTimeout(() => {
         next.click();
     }, timeAutoNext)
 }
-
+ 
 //navbar
-
+ 
 const toggleBtn = document.querySelector('.toggle_btn')
 const toggleBtnIcon = document.querySelector('.toggle_btn i')
 const dropDownMenu = document.querySelector('.dropdown_menu')
-
+ 
 toggleBtn.onclick = function () {
     dropDownMenu.classList.toggle('open')
     const isOpen = dropDownMenu.classList.contains('open')
-
+ 
     toggleBtnIcon.classList = isOpen
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
+}
+ 
+//modo noturno
+ 
+let darkmode = document.querySelector('#darkmode');
+ 
+darkmode.onclick = () => {
+    if(darkmode.classList.contains('fa-moon')){
+        darkmode.classList.replace('fa-moon', 'fa-sun');
+        document.body.classList.add('color');
+    }else{
+        darkmode.classList.replace('fa-sun', 'fa-moon');
+        document.body.classList.remove('color')
+    }
 }
