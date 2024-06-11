52fca147f99739bef7f949b17346872b110a9f6b
SophiaAg <sophiafroez7@gmail.com>
2024-06-10 23:24:26 -0300

    tudoerrado

--- js/Equipe.js
+++ js/Equipe.js
@@ -1,28 +1,55 @@
 //equipe
 
-const Equipecarousel = document.querySelector(".Equipecarousel");
+//const Equipecarousel = document.querySelector(".Equipecarousel");
 
-let isDragging = false, startX, startScrollLeft;
+//let isDragging = false, startX, startScrollLeft;
 
-const dragStart = (e) => {
-    isDragging = true;
-    Equipecarousel.classList.add("dragging");
+//const dragStart = (e) => {
+    //isDragging = true;
+    //Equipecarousel.classList.add("dragging");
     
-    startX = e.pagex;
-    startScrollLeft = Equipecarousel.scrollLeft
+    //startX = e.pagex;
+    //startScrollLeft = Equipecarousel.scrollLeft
 
     
+//}
+
+//const dragging = (e) => {
+  //  console.log(e.pageX)
+//}
+
+//const dragStop = () => {
+  //  isDragging = false;
+    //Equipecarousel.classList.remove("dragging");
+//}
+
+//Equipecarousel.addEventListener("mousedown" , dragStart);
+//Equipecarousel.addEventListener("mousemove" , dragging);
+//Equipecarousel.addEventListener("mouseup" , dragStop);
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+document.querySelector('.next').addEventListener('click', () => {
+    changeSlide(1);
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    changeSlide(-1);
+});
+
+function changeSlide(direction) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+    slides[currentSlide].classList.add('active');
 }
 
-const dragging = (e) => {
-    console.log(e.pageX)
-}
+// Inicializa o primeiro slide como ativo
+slides[currentSlide].classList.add('active');
 
-const dragStop = () => {
-    isDragging = false;
-    Equipecarousel.classList.remove("dragging");
-}
-
-Equipecarousel.addEventListener("mousedown" , dragStart);
-Equipecarousel.addEventListener("mousemove" , dragging);
-Equipecarousel.addEventListener("mouseup" , dragStop);
+// Auto slide change every 5 seconds
+setInterval(() => {
+    changeSlide(1);
+}, 5000);
