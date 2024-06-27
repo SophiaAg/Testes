6bcb48c9cd0e52b233150dc0c194ba75e5336fdf
SophiaAg <sophiafroez7@gmail.com>
2024-06-27 20:11:50 -0300

    20h11

--- js/empresaspremium.js
+++ js/empresaspremium.js
@@ -1,72 +1,47 @@
-const slideContainer = document.querySelector('.container-empresaPremium');
-const slide = document.querySelector('.slidesrow');
-const nextBtn = document.getElementById('next-btn');
-const prevBtn = document.getElementById('prev-btn');
-const interval = 3000;
 
-let slides = document.querySelectorAll('.slide-empresaP');
-let index = 1;
-let slideId;
 
-const firstClone = slides[0].cloneNode(true);
-const lastClone = slides[slides.length - 1].cloneNode(true);
+let nextDom = document.getElementById('next-btn');
+let prevDom = document.getElementById('prev-btn');
+ 
+let carouselDom = document.querySelector('.container-empresaPremium');
+let SliderDom = carouselDom.querySelector('.container-empresaPremium .slidesrow');
 
-firstClone.id = 'first-clone';
-lastClone.id = 'last-clone';
-
-slide.append(firstClone);
-slide.prepend(lastClone);
-
-const slideWidth = slides[index].clientWidth;
-
-slide.style.transform = `translateX(${-slideWidth * index}px)`;
-
-console.log(slides);
-
-const startSlide = () => {
-  slideId = setInterval(() => {
-    moveToNextSlide();
-  }, interval);
-};
-
-const getSlides = () => document.querySelectorAll('.slide-empresaP');
-
-slide.addEventListener('transitionend', () => {
-  slides = getSlides();
-  if (slides[index].id === firstClone.id) {
-    slide.style.transition = 'none';
-    index = 1;
-    slide.style.transform = `translateX(${-slideWidth + index}px)`;
-  }
-
-  if (slides[index].id === lastClone.id) {
-    slide.style.transition = 'none';
-    index = slides.length - 2;
-    slide.style.transform = `translateX(${-slideWidth * index}px)`;
-  }
-});
-
-const moveToNextSlide = () => {
-  slides = getSlides();
-  if (index >= slides.length - 1) return;
-  index++;
-  slide.style.transition = '.7s ease-out';
-  slide.style.transform = `translateX(${-slideWidth * index}px)`;
-};
-
-const moveToPreviousSlide = () => {
-  if (index <= 0) return;
-  index--;
-  slide.style.transition = '.7s ease-out';
-  slide.style.transform = `translateX(${-slideWidth * index}px)`;
-};
-
-slideContainer.addEventListener('mouseenter', () => {
-  clearInterval(slideId);
-});
-
-slideContainer.addEventListener('mouseleave', startSlide);
-nextBtn.addEventListener('click', moveToNextSlide);
-prevBtn.addEventListener('click', moveToPreviousSlide);
-
-startSlide();
+ 
+let timeDom = document.querySelector('.container-empresaPremium .time');
+ 
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+ 
+nextDom.onclick = function(){
+    showSlider('next-btn');    
+}
+ 
+prevDom.onclick = function(){
+    showSlider('prev-btn');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.container-empresaPremium .slidesrow .slide-empresaP');
+   
+   
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        carouselDom.classslidesrow.add('next-btn');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        carouselDom.classList.add('prev-btn');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next-btn');
+        carouselDom.classList.remove('prev-btn');
+    }, timeRunning);
+ 
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
