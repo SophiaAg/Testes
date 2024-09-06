7426eed889ad729a565d9e5c583b2838fdae4c72
SophiaAg <sophiafroez@gmail.com>
2024-09-06 10:06:24 -0300

    10h10

--- cadastroempresa/infopessoal.js
+++ cadastroempresa/infopessoal.js
@@ -14,7 +14,7 @@ let current = 1;
 
 nextBtnFirst.addEventListener("click", function(event){
   event.preventDefault();
-  slidePage.style.marginLeft = "-25%";
+  slidePage.style.marginLeft = "-100%";
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
@@ -22,7 +22,7 @@ nextBtnFirst.addEventListener("click", function(event){
 });
 nextBtnSec.addEventListener("click", function(event){
   event.preventDefault();
-  slidePage.style.marginLeft = "-50%";
+  slidePage.style.marginLeft = "-200%";
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
@@ -30,7 +30,7 @@ nextBtnSec.addEventListener("click", function(event){
 });
 nextBtnThird.addEventListener("click", function(event){
   event.preventDefault();
-  slidePage.style.marginLeft = "-75%";
+  slidePage.style.marginLeft = "-300%";
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
@@ -57,7 +57,7 @@ prevBtnSec.addEventListener("click", function(event){
 });
 prevBtnThird.addEventListener("click", function(event){
   event.preventDefault();
-  slidePage.style.marginLeft = "-25%";
+  slidePage.style.marginLeft = "-100%";
   bullet[current - 2].classList.remove("active");
   progressCheck[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
@@ -65,7 +65,7 @@ prevBtnThird.addEventListener("click", function(event){
 });
 prevBtnFourth.addEventListener("click", function(event){
   event.preventDefault();
-  slidePage.style.marginLeft = "-50%";
+  slidePage.style.marginLeft = "-200%";
   bullet[current - 2].classList.remove("active");
   progressCheck[current - 2].classList.remove("active");
   progressText[current - 2].classList.remove("active");
