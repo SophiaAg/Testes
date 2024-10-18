f170192cd9e214db7a9e90d4aa1869ca5b133ae3
SophiaAg <sophiafroez7@gmail.com>
2024-10-17 21:50:42 -0300

    21h50

--- js/dashboard-menu.js
+++ js/dashboard-menu.js
@@ -1,6 +1,7 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const sidebarItems = document.querySelectorAll('.container .menulateral .slidebar .item');
 
 menuBtn.addEventListener('click', () =>{ 
     sideMenu.style.display = 'block';
@@ -9,3 +10,19 @@ menuBtn.addEventListener('click', () =>{
     closeBtn.addEventListener('click', () => {
         sideMenu.style.display = 'none';
     })
+
+    //active
+
+    let activeItem = sidebarItems[0];
+
+sidebarItems.forEach(element => {
+    element.addEventListener('click', () => {
+        if (activeItem) {
+            activeItem.removeAttribute('id');
+        }
+
+        element.setAttribute('id', 'active');
+        activeItem = element;
+
+    });
+});
